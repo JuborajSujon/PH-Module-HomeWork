5bc14ffc116a6bd6f1ca8924f58ad80204f4da41
Juboraj Sujon <mdsujon72@gmail.com>
2024-03-09 12:47:23 +0600

    Different way to handle events in react

--- Milestone 7/react-core2/src/App.jsx
+++ Milestone 7/react-core2/src/App.jsx
@@ -1,9 +1,29 @@
 import "./App.css";
 
 function App() {
+  function handleClick() {
+    alert("Button Clicked");
+  }
+
+  const handleClick2 = () => {
+    alert("Button 2 Clicked");
+  };
+
+  const addToFive = (num) => {
+    alert(num + 5);
+  };
   return (
     <>
       <h3>React Core Concepts 2</h3>
+      <button onClick={handleClick}>Click Me</button>
+      <button onClick={handleClick}>Click Me 2</button>
+      <button
+        onClick={() => {
+          alert("Button 3 Clicked");
+        }}>
+        Click Me 3
+      </button>
+      <button onClick={() => addToFive(5)}>Add to Five</button>
     </>
   );
 }
