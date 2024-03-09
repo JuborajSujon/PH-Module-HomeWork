e64f86ba22011434b12470ac1d71fcaa5d8fd6c8
Juboraj Sujon <mdsujon72@gmail.com>
2024-03-09 19:15:56 +0600

    Understand the concept of useEffect

--- Milestone 7/react-core2/src/App.jsx
+++ Milestone 7/react-core2/src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Counter from "./Counter";
 import Team from "./Team";
+import Users from "./Users";
 
 function App() {
   function handleClick() {
@@ -17,6 +18,7 @@ function App() {
   return (
     <>
       <h3>React Core Concepts 2</h3>
+      <Users></Users>
       <Team></Team>
       <Counter />
       <button onClick={handleClick}>Click Me</button>
