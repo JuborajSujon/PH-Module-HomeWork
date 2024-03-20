e4d8d1751e64273d84befe474a151f3c20d6ad63
Juboraj Sujon <mdsujon72@gmail.com>
2024-03-20 20:48:21 +0600

    adding some router

--- Milestone 8/intro-to-react-router/src/main.jsx
+++ Milestone 8/intro-to-react-router/src/main.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/about",
+    element: <div>About</div>,
+  },
+  {
+    path: "/services",
+    element: <div>Services</div>,
+  },
+  {
+    path: "/contact",
+    element: <div>Contact</div>,
+  },
+  {
+    path: "*",
+    element: <div>Not Found</div>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
