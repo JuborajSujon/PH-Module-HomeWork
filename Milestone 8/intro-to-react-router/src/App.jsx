ec5e6bb8fd6655a4272652705d0376766eea3b42
Juboraj Sujon <mdsujon72@gmail.com>
2024-03-21 23:46:57 +0600

    loading spinner show by using useNavigation

--- Milestone 8/intro-to-react-router/src/App.jsx
+++ Milestone 8/intro-to-react-router/src/App.jsx
@@ -1,12 +1,14 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 
 function App() {
+  const navigation = useNavigation();
   return (
     <>
       <Header />
+      {navigation.state === "loading" ? <p>Loading...</p> : null}
       <Outlet />
       <Footer />
     </>
