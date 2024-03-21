aecf7d48bfddddd0728b7507c26909dae3dc4041
Juboraj Sujon <mdsujon72@gmail.com>
2024-03-21 23:49:34 +0600

    find location object by using useLocation

--- Milestone 8/intro-to-react-router/src/App.jsx
+++ Milestone 8/intro-to-react-router/src/App.jsx
@@ -1,10 +1,12 @@
-import { Outlet, useNavigation } from "react-router-dom";
+import { Outlet, useLocation, useNavigation } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 
 function App() {
   const navigation = useNavigation();
+  const location = useLocation();
+  console.log(location);
   return (
     <>
       <Header />
