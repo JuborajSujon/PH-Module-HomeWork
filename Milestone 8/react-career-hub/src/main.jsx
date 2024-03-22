9c1e65e07fe42bf9163a8067d2cd08e98b509314
Juboraj Sujon <mdsujon72@gmail.com>
2024-03-22 23:03:01 +0600

    Create Applied jobs components

--- Milestone 8/react-career-hub/src/main.jsx
+++ Milestone 8/react-career-hub/src/main.jsx
@@ -5,6 +5,7 @@ import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from "./components/Root/Root";
 import Home from "./components/Home/Home";
+import AppliedJobs from "./components/Applied Jobs/AppliedJobs";
 
 const router = createBrowserRouter([
   {
@@ -14,6 +15,10 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Home />,
+      },
+      {
+        path: "/applied",
+        element: <AppliedJobs />,
       },
     ],
   },
