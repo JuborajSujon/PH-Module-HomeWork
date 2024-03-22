5d73965316423b558362c1084452138eecb0276a
Juboraj Sujon <mdsujon72@gmail.com>
2024-03-22 23:26:27 +0600

    feature jobs data fetch

--- Milestone 8/react-career-hub/src/components/FeaturedJobs/FeaturedJobs.jsx
+++ Milestone 8/react-career-hub/src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -1,13 +1,24 @@
-import React from "react";
+import { useEffect, useState } from "react";
 
 const FeaturedJobs = () => {
+  const [jobs, setJobs] = useState([]);
+  useEffect(() => {
+    fetch("jobs.json")
+      .then((res) => res.json())
+      .then((data) => setJobs(data));
+  }, []);
   return (
     <div>
-      <h2 className="text-4xl font-bold text-center py-5">Featured Jobs</h2>
-      <p className="text-center">
-        Explore thousands of job oppertunities with all the information you
-        need. Its your future
-      </p>
+      <div>
+        <h2 className="text-4xl font-bold text-center py-5">Featured Jobs</h2>
+        <p className="text-center">
+          Explore thousands of job oppertunities with all the information you
+          need. Its your future
+        </p>
+      </div>
+      <div>
+        {jobs.map(job=>)}
+      </div>
     </div>
   );
 };
