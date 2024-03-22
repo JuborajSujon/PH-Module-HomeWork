1508750412c1badb9b75381f2aa7a82a04dcdb8c
Juboraj Sujon <mdsujon72@gmail.com>
2024-03-22 11:33:17 +0600

    get value from input by using name attribute

---
+++ Milestone 8/intro-form/src/components/Simple Form/SimpleForm.jsx
@@ -0,0 +1,24 @@
+const SimpleForm = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target.name.value);
+    console.log(e.target.email.value);
+    console.log(e.target.phone.value);
+    console.log("form submitted");
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" placeholder="Enter your name" />
+        <br />
+        <input type="text" name="email" placeholder="Enter your email" />
+        <br />
+        <input type="number" name="phone" placeholder="Enter your phone" />
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+};
+
+export default SimpleForm;
