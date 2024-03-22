cdb29aea17c83e4cf90e4c37fba78dfeb76bc829
Juboraj Sujon <mdsujon72@gmail.com>
2024-03-22 21:42:22 +0600

    Define context api inside grandpa

--- Milestone 8/intro-form/src/components/Grandpa/Grandpa.jsx
+++ Milestone 8/intro-form/src/components/Grandpa/Grandpa.jsx
@@ -2,19 +2,29 @@ import Dad from "../Dad/Dad";
 import Uncle from "../Uncle/Uncle";
 import Aunty from "../Anuty/Aunty";
 import "./Grandpa.css";
+import { createContext } from "react";
+const AssetContext = createContext("Gold");
 
 const Grandpa = () => {
   const asset = "diamond";
   return (
     <div className="grandpa">
       <h2>Grandpa</h2>
-      <section className="flex">
-        <Dad asset={asset} />
-        <Uncle asset={asset} />
-        <Aunty />
-      </section>
+      <AssetContext.Provider value={asset}>
+        <section className="flex">
+          <Dad asset={asset} />
+          <Uncle asset={asset} />
+          <Aunty />
+        </section>
+      </AssetContext.Provider>
     </div>
   );
 };
 
 export default Grandpa;
+
+/**
+ * 1. Create a context and export it
+ * 2. Add provider for the context with a value
+ * 3.
+ */
