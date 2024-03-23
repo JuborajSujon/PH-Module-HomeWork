f3c2ba911960426532db0069cc9b8dd7d1de0370
Juboraj Sujon <mdsujon72@gmail.com>
2024-03-23 10:18:30 +0600

    handle show all jobs

--- Milestone 8/react-career-hub/src/components/FeaturedJobs/FeaturedJobs.jsx
+++ Milestone 8/react-career-hub/src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -3,6 +3,9 @@ import Job from "../Job/Job";
 
 const FeaturedJobs = () => {
   const [jobs, setJobs] = useState([]);
+
+  // this is not the best way to load show all data
+  const [dataLength, setDataLength] = useState(4);
   useEffect(() => {
     fetch("jobs.json")
       .then((res) => res.json())
@@ -18,9 +21,19 @@ const FeaturedJobs = () => {
         </p>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
-        {jobs.map((job) => (
+        {jobs.slice(0, dataLength).map((job) => (
           <Job key={job.id} job={job}></Job>
         ))}
+      </div>
+      <div
+        className={`${
+          dataLength === jobs.length ? "hidden" : "block"
+        } text-center py-8`}>
+        <button
+          className="btn btn-primary"
+          onClick={() => setDataLength(jobs.length)}>
+          Show All
+        </button>
       </div>
     </div>
   );
--- Milestone 8/react-career-hub/src/components/Job/Job.jsx
+++ Milestone 8/react-career-hub/src/components/Job/Job.jsx
@@ -1,3 +1,5 @@
+import { MdLocationOn } from "react-icons/md";
+import { AiOutlineDollar } from "react-icons/ai";
 const Job = ({ job }) => {
   const {
     logo,
@@ -10,8 +12,8 @@ const Job = ({ job }) => {
   } = job;
   return (
     <div className="card card-compact bg-base-100 shadow-xl">
-      <figure>
-        <img src={logo} alt={job_title} />
+      <figure className="py-4">
+        <img className="w-32" src={logo} alt={job_title} />
       </figure>
       <div className="card-body">
         <h2 className="card-title">{job_title}</h2>
@@ -23,6 +25,15 @@ const Job = ({ job }) => {
           <button className="btn font-extrabold border rounded border-violet-400 mr-4 text-violet-400">
             {job_type}
           </button>
+        </div>
+        <div className="my-4 flex gap-6">
+          <h2 className="flex items-center text-xl">
+            <MdLocationOn className=" mr-2" /> {location}
+          </h2>
+          <h2 className="flex items-center text-xl">
+            <AiOutlineDollar className=" mr-2" />
+            {salary}
+          </h2>
         </div>
         <div className="card-actions">
           <button className="btn btn-primary">View Details</button>
