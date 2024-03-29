65872ea5cbaae83f170a611bbe9b8b58933b1b4e
Juboraj Sujon <mdsujon72@gmail.com>
2024-03-29 20:09:09 +0600

    project inital for simple firebase

---
+++ Milestone 9/simple-firebase/src/_steps.js
@@ -0,0 +1,8 @@
+/**
+ * 1. visit: console.firebase.google.com
+ * 2. create project (skip google analytics)
+ * 3. register app (create config)
+ * 4. install firebase : npm i firebase
+ * 5. Add config file to your project
+ * 6. DANGER: Do not publish or make firebase config to public by pushing those to github
+ */
---
+++ Milestone 9/simple-firebase/src/firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAos9SRWHOXZp1l60VE00zwrNPi7m6J7Cs",
+  authDomain: "simple-firebase-2aa78.firebaseapp.com",
+  projectId: "simple-firebase-2aa78",
+  storageBucket: "simple-firebase-2aa78.appspot.com",
+  messagingSenderId: "483439786270",
+  appId: "1:483439786270:web:a9c11e2de44ae80b1c710f",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
