3a86119cc7f0e99de6c78ca217ffd1cf7d4f58cb
Juboraj Sujon <mdsujon72@gmail.com>
2024-03-29 20:13:39 +0600

    create simple route layout

--- Milestone 9/simple-firebase/src/main.jsx
+++ Milestone 9/simple-firebase/src/main.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainLayout from "./layout/MainLayout.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
