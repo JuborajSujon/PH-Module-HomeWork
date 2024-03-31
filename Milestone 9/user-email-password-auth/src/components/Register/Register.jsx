114cb215c460b69df1c3be6ead53bfac25ee7452
Juboraj Sujon <mdsujon72@gmail.com>
2024-03-31 12:56:36 +0600

    create an register form

--- Milestone 9/user-email-password-auth/src/components/Register/Register.jsx
+++ Milestone 9/user-email-password-auth/src/components/Register/Register.jsx
@@ -1,14 +1,32 @@
 const Register = () => {
   return (
-    <div className="flex flex-col items-center ">
-      <h2 className="text-xl">Please Register</h2>
-      <form className="mt-4 space-y-4">
-        <input type="email" name="email" placeholder="email" />
-        <br />
-        <input type="password" name="password" placeholder="password" />
-        <br />
-        <input className="btn btn-primary" type="submit" value="Register" />
-      </form>
+    <div className="flex flex-col items-center">
+      <div className="w-1/2 mx-auto border-2 border-blue-500 p-5">
+        <h2 className="text-2xl">Please Register</h2>
+        <form className="mt-4 space-y-4">
+          <input
+            className="w-full input"
+            type="email"
+            name="email"
+            placeholder="Email"
+          />
+          <br />
+          <input
+            className="w-full input"
+            type="password"
+            name="password"
+            placeholder="password"
+          />
+          <br />
+          <div className="text-center">
+            <input
+              className="btn btn-primary w-1/2"
+              type="submit"
+              value="Register"
+            />
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
