f5583ad9312b3026100c202ff20c5522f0468bdf
Juboraj Sujon <mdsujon72@gmail.com>
2024-04-01 21:16:39 +0600

    useEffect clean up function

--- Milestone 9/private-route/src/components/Home/Home.jsx
+++ Milestone 9/private-route/src/components/Home/Home.jsx
@@ -1,5 +1,50 @@
+import { useEffect, useState } from "react";
+
 const Home = () => {
-  return <div>This is Home. </div>;
+  const [toggle, setToggle] = useState(false);
+  return (
+    <>
+      <div className="text-center">
+        <button
+          onClick={() => setToggle(!toggle)}
+          className="btn btn-sm btn-success">
+          Toggle
+        </button>
+      </div>
+      <div className="text-center my-12">{toggle && <MyComponent />}</div>
+    </>
+  );
+};
+
+const MyComponent = () => {
+  // useEffect with dependency array
+  // use handle scroll with cleanup function
+  useEffect(() => {
+    const handleScroll = () => {};
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  // useEffect with call back and dependency array
+  // Use setInervalfn to clear the interval
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     console.log("inside component");
+  //   }, 1000);
+
+  //   return () => clearInterval(interval);
+  // }, []);
+
+  return (
+    <div>
+      <h2 className="text-xl text-red-500">
+        Conditional rendering is working ....!
+      </h2>
+    </div>
+  );
 };
 
 export default Home;
