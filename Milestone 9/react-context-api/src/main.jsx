ef13bcb18df26a1df0a6945a41d3f52180fe7a74
Juboraj Sujon <mdsujon72@gmail.com>
2024-04-02 13:09:41 +0600

    define our main route as Root component

--- Milestone 9/react-context-api/src/main.jsx
+++ Milestone 9/react-context-api/src/main.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+import Root from "./components/Root/Root";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <h1 className="text-3xl font-bold underline">Hello world!</h1>,
+    element: <Root />,
   },
 ]);
 
