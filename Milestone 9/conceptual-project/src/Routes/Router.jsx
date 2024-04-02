bb8f495db5eef64041e54b3bfde06fa8f830891b
Juboraj Sujon <mdsujon72@gmail.com>
2024-04-02 22:39:10 +0600

    enviroment variable define

---
+++ Milestone 9/conceptual-project/src/Routes/Router.jsx
@@ -0,0 +1,27 @@
+import { createBrowserRouter } from "react-router-dom";
+import Root from "../components/Root/Root";
+import Home from "../components/Home/Home";
+import Login from "../components/Login/Login";
+import Register from "../components/Register/Register";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+    ],
+  },
+]);
+
+export default router;
