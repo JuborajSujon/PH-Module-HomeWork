f09c36816f8a9ac71c4d3769355672c26e88b73c
Juboraj Sujon <mdsujon72@gmail.com>
2024-04-02 13:05:38 +0600

    create basic router

--- Milestone 9/react-context-api/src/main.jsx
+++ Milestone 9/react-context-api/src/main.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1 className="text-3xl font-bold underline">Hello world!</h1>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
